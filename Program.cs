e1108a99b935b471683a98b397ad0a748321e1dd
SergeyMironov <indigod@bk.ru>
2019-10-23 12:04:51 +0400

    First commit in local repository

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestOfGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
